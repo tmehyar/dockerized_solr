b49f837648550dd10b33919e4bf231a8c7365fc2
tamer.mehyar <tamer.mehyar@gmail.com>
2021-10-21 13:06:49 -0500

    Included docker-compose + config files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dockerized_solr
 Docker configuration for running solr in a container
+
+# solr 7.5.0
+Place the solr cores folders in this folder and run `docker-compose up` in this folder to start solr.
+
+You can edit the docker-compose file to use different port mappings.
+
+`solr.xml` was taken from the cores folder of a Solr installation.
+
+The environment variables in the docker-compose file were taken from `solr.in.sh` found in the `bin` folder of a Solr installation.
---
+++ setupssh.sh
@@ -0,0 +1,4 @@
+keytool -genkeypair -alias solr-ssl -keyalg RSA -keysize 2048 -keypass secret -storepass secret -validity 9999 -keystore solr-ssl.keystore.jks -ext SAN=DNS:localhost,IP:192.168.1.3,IP:127.0.0.1 -dname "CN=localhost, OU=Organizational Unit, O=Organization, L=Location, ST=State, C=Country";
+keytool -importkeystore -srckeystore solr-ssl.keystore.jks -destkeystore solr-ssl.keystore.p12 -srcstoretype jks -deststoretype pkcs12 -keypass secret -storepass secret -srcstorepass secret -noprompt;
+openssl pkcs12 -in solr-ssl.keystore.p12 -out solr-ssl.pem -passin pass:secret -passout pass:secret;
+cp ./solr-ssl.keystore.jks /opt/solr/server/etc;
